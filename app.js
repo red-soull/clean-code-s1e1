fad4b4aa479124775fd9c5bdb4d223cddc2cfc98
red-soull <rijaiaola@gmail.com>
2021-11-30 00:14:53 +0300

    refactor: changed class names to bem according to the rules 3.1, 3.2, 3.3 in html-and-css.md and 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="key key-button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
