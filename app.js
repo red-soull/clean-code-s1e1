ad32ff8d115c768381776934379f10ef7a9b80d3
red-soull <rijaiaola@gmail.com>
2021-11-30 00:30:57 +0300

    refactor: changed class names to bem according to rules 3.1, 3.2, 3.3 in html-and-css.md and 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -33,12 +33,13 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='list__tasks_label task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="list__tasks_checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="list__tasks_input task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="key key-button edit";
